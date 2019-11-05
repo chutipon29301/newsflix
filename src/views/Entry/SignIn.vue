e6c947bb5337c4488205fa89c1dd3a2be9d24cb3
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-11-06 00:03:09 +0700

    :sparkles: Add v model to input

--- src/views/Entry/SignIn.vue
+++ src/views/Entry/SignIn.vue
@@ -5,22 +5,24 @@
       class="shadow appearance-none rounded w-full py-4 px-3 mb-4 text-white leading-tight focus:outline-none focus:shadow-outline bg-input text-lg"
       placeholder="Email or phone number"
       type="text"
+      v-model="username"
     />
     <input
       class="shadow appearance-none rounded w-full py-4 px-3 mb-12 text-white leading-tight focus:outline-none focus:shadow-outline bg-input text-lg"
       placeholder="Password"
       type="password"
+      v-model="password"
     />
     <button
       class="shadow rounded w-full py-4 px-3 mb-4 text-white leading-tight focus:outline-none focus:shadow-outline bg-btnred text-lg hover:bg-red-800 active:bg-red-900"
-    >
-      Sign In
-    </button>
+      @click="signIn"
+    >Sign In</button>
     <div class="flex justify-between mb-64">
       <label class="inline-flex items-center">
         <input
           type="checkbox"
           class="form-checkbox border-none bg-input text-inputblack"
+          v-model="rememberUser"
         />
         <span class="ml-2 text-inputblack">Remember me</span>
       </label>
@@ -36,5 +38,15 @@
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 @Component
-export default class Login extends Vue {}
+export default class Login extends Vue {
+  private username: string = "";
+  private password: string = "";
+  private rememberUser: boolean = false;
+
+  private signIn() {
+    if (this.username === "abc@gmail.com" && this.password === "password") {
+      console.log("Redirect");
+    }
+  }
+}
 </script>
--- src/views/Entry/SignUp.vue
+++ src/views/Entry/SignUp.vue
@@ -5,33 +5,47 @@
       class="shadow appearance-none rounded w-full py-4 px-3 mb-4 text-white leading-tight focus:outline-none focus:shadow-outline bg-input text-lg"
       placeholder="Name (e.g. John Doe)"
       type="text"
+      v-model="name"
     />
     <input
       class="shadow appearance-none rounded w-full py-4 px-3 mb-4 text-white leading-tight focus:outline-none focus:shadow-outline bg-input text-lg"
       placeholder="Email"
       type="text"
+      v-model="email"
     />
     <input
       class="shadow appearance-none rounded w-full py-4 px-3 mb-4 text-white leading-tight focus:outline-none focus:shadow-outline bg-input text-lg"
       placeholder="Password (8-20 characters)"
       type="password"
+      v-model="password"
     />
     <input
       class="shadow appearance-none rounded w-full py-4 px-3 mb-12 text-white leading-tight focus:outline-none focus:shadow-outline bg-input text-lg"
       placeholder="Confirm Password"
       type="password"
+      v-model="confirmPassword"
     />
     <button
       class="shadow rounded w-full py-4 px-3 mb-40 text-white leading-tight focus:outline-none focus:shadow-outline bg-btnred text-lg hover:bg-red-800 active:bg-red-900"
+      @click="createAccount"
     >Create account</button>
     <router-link to="signin" class="hover:underline">
       <font-awesome-icon icon="arrow-left" class="mr-2" />Sign In
-      </router-link>
+    </router-link>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 @Component
-export default class SignUp extends Vue {}
+export default class SignUp extends Vue {
+  private name: string = "";
+  private email: string = "";
+  private password: string = "";
+  private confirmPassword: string = "";
+
+  private createAccount() {
+    console.log("Create Account");
+  }
+}
 </script>
