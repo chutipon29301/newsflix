9689812d2dd2659db95fb282219fd35a4ecfe341
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-11-05 14:41:09 +0700

    :sparkles: Add sign up button

--- src/views/Entry/SignIn.vue
+++ src/views/Entry/SignIn.vue
@@ -13,18 +13,17 @@
     />
     <button
       class="shadow rounded w-full py-4 px-3 mb-4 text-white leading-tight focus:outline-none focus:shadow-outline bg-btnred text-lg hover:bg-red-800 active:bg-red-900"
-    >
-      Sign In
-    </button>
-    <div class="flex justify-between">
+    >Sign In</button>
+    <div class="flex justify-between mb- mb-56">
       <label class="inline-flex items-center">
-        <input
-          type="checkbox"
-          class="form-checkbox border-none bg-input text-inputblack"
-        />
+        <input type="checkbox" class="form-checkbox border-none bg-input text-inputblack" />
         <span class="ml-2 text-inputblack">Remember me</span>
       </label>
       <a href="#" class="text-inputblack hover:underline">Need help ?</a>
+    </div>
+    <div class="flex">
+      <div class="text-inputblack mr-4">New to Newsflix ?</div>
+      <a href="#" class="hover:underline">Sign up now</a>
     </div>
   </div>
 </template>
