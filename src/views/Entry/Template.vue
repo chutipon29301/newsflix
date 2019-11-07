d49c0044fae34f2ef7cde573511f8b3e0f6130a9
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-11-07 22:42:24 +0700

    :iphone: Change sign in box margin

--- src/views/Entry/Template.vue
+++ src/views/Entry/Template.vue
@@ -7,10 +7,10 @@
     <vue-particles class="fixed select-none top-0 left-0 h-full w-full" />
     <div class="flex flex-col h-screen w-full items-center sm:items-start">
       <router-link to="/" class="z-10 mt-4 mb-8 sm:mb-0">
-        <img src="../../assets/logo.png" class="z-10" />
+        <img src="../../assets/logo.png" class="z-10 select-none" />
       </router-link>
       <div class="flex h-screen w-full justify-center items-center">
-        <div class="z-10 w-full max-w-md bg-black text-white py-8 px-8 sm:px-12 mx-8">
+        <div class="z-10 w-full max-w-md bg-black text-white py-8 px-8 sm:px-12 mx-4">
           <vue-page-transition>
             <router-view />
           </vue-page-transition>
