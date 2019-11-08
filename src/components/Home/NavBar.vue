bbed4a15b6d6d4317b7b07cfacca721670a9284d
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-11-08 23:25:55 +0700

    :art: Disable user select on logo

--- src/components/Home/NavBar.vue
+++ src/components/Home/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex w-full shadow justify-between pr-4 bg-white">
     <div class="flex items-center flex-1">
-      <img src="../../assets/logo.png" class="w-48" />
+      <img src="../../assets/logo.png" class="w-48 select-none" />
       <div class="inline-block relative w-64 flex-1 mr-48">
         <vAutocomplete
           class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
