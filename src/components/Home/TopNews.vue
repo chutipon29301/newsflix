b0edbfd166fcc4c1f049ddbc1c58c556240c9c8a
namking111 <namking_achiraya@hotmail.com>
2019-11-11 19:02:46 +0700

    :bento: Add image in TopNews

--- src/components/Home/TopNews.vue
+++ src/components/Home/TopNews.vue
@@ -1,17 +1,63 @@
 <template>
-  <div>
+  <div class="max-w-sm">
     <div class="text-white font-semibold text-xl">TOP 5</div>
-    <div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-      <div>Hello</div>
-    </div>
+    <SmallNewsItem
+      v-for="(news, index) in newsSources"
+      :key="index"
+      :title="news.title"
+      :img="news.img"
+      :detail="news.detail"
+      class="w-full"
+    >{{news.date}}</SmallNewsItem>
   </div>
 </template>
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-@Component
-export default class TopNews extends Vue {}
+import SmallNewsItem from "@/components/Home/SmallNewsItem.vue";
+@Component({
+  components: {
+    SmallNewsItem
+  }
+})
+export default class TopNews extends Vue {
+  private newsSources: any[] = [
+    {
+      title: "Hong Kong police officer shoots protester",
+      img: "top5_1.png",
+      detail: "A 21-year-old protester was shot with a live round by a police officer.",
+      date: "11 November 2019"
+    },
+    {
+      title: "Sydney under 'catastrophic' warning from dozens of bushfires ",
+      img: "top5_2.png",
+      detail: "Fire officials are warning of \"catastrophic fire danger\"",
+      date: "11 November 2019"
+    },
+    {
+      title: "Hong Kong police officer shoots protester",
+      img: "top5_1.png",
+      detail: "A 21-year-old protester was shot with a live round by a police officer.",
+      date: "11 November 2019"
+    },
+    {
+      title: "Hong Kong police officer shoots protester",
+      img: "top5_1.png",
+      detail: "A 21-year-old protester was shot with a live round by a police officer.",
+      date: "11 November 2019"
+    },
+    {
+      title: "Hong Kong police officer shoots protester",
+      img: "top5_1.png",
+      detail: "A 21-year-old protester was shot with a live round by a police officer.",
+      date: "11 November 2019"
+    }
+  ];
+  // private dates: string[] = [
+  //   "11 November 2019",
+  //   "11 November 2019",
+  //   "8 November 2019",
+  //   "5 November 2019",
+  //   "2 November 2019"
+  // ];
+}
 </script>
