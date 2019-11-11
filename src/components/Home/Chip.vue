b3e9e1d191482ac16179e3f329f96d35f3077dc6
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-11-11 17:34:01 +0700

    :sparkles: Disable user select on chip

--- src/components/Home/Chip.vue
+++ src/components/Home/Chip.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="bg-red-700 hover:bg-red-800 active:bg-red-900 shadow-lg px-8 py-2 rounded-full text-white"
+    class="bg-red-700 hover:bg-red-800 active:bg-red-900 shadow-lg px-8 py-2 rounded-full text-white select-none"
   >
     #<slot />
   </div>
