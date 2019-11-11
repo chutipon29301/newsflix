9fe786813a83b0a7304be10b71876a1339f831d2
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-11-11 16:28:16 +0700

    :recycle: Refactor popular tags

---
+++ src/components/Home/PopularTags.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="pt-4 pb-8">
+    <div class="flex items-center mb-4">
+      <font-awesome-icon icon="tags" class="mr-4" />
+      <div class="text-xl font-semibold">Popular Tags</div>
+    </div>
+    <div class="flex">
+      <Chip v-for="chip in chips" :key="chip" class="mr-4">{{chip}}</Chip>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Chip from "@/components/Home/Chip.vue";
+@Component({
+  components: { Chip }
+})
+export default class PopularTags extends Vue {
+  private chips: string[] = ["Apple", "YouTube"];
+}
+</script>
--- src/views/Home/Index.vue
+++ src/views/Home/Index.vue
@@ -32,14 +32,8 @@
       </div>
     </div>
     <div class="w-full bg-inputhover">
-      <div class="container py-4">
-        <div class="flex items-center mb-4">
-          <font-awesome-icon icon="tags" class="mr-4" />
-          <div class="text-xl font-semibold">Popular Tags</div>
-        </div>
-        <div class="flex">
-          <Chip v-for="chip in chips" :key="chip" class="mr-4">{{chip}}</Chip>
-        </div>
+      <div class="container">
+        <PopularTags/>
       </div>
     </div>
   </div>
@@ -48,10 +42,13 @@
 import { Component, Vue } from "vue-property-decorator";
 import SmallNewsItem from "@/components/Home/SmallNewsItem.vue";
 import Chip from "@/components/Home/Chip.vue";
+import PopularTags from "@/components/Home/PopularTags.vue";
+
 @Component({
   components: {
     SmallNewsItem,
-    Chip
+    Chip,
+    PopularTags
   }
 })
 export default class Index extends Vue {}
