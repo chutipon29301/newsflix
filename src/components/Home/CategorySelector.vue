265be83c124e7cb1d69c908dd31b16e7966a67a8
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-11-23 23:29:41 +0700

    :sparkles: Add category selector button

--- src/components/Home/CategorySelector.vue
+++ src/components/Home/CategorySelector.vue
@@ -1,9 +1,19 @@
 <template>
-    <div>This is categories component</div>
+  <div class="flex w-2/5 text-gray-700">
+    <button
+      v-for="category in categories"
+      :key="category"
+      :class="`flex-1 rounded ${selectedCategory === category ? 'font-bold bg-gray-400 text-black' : 'hover:bg-gray-200 hover:font-semibold active:bg-gray-300'}`"
+      @click="selectedCategory = category"
+    >{{category}}</button>
+  </div>
 </template>
 
 <script lang="ts">
-import {Component, Vue } from "vue-property-decorator";
+import { Component, Vue } from "vue-property-decorator";
 @Component
-export default class CategorySelector extends Vue {};
+export default class CategorySelector extends Vue {
+  private categories: string[] = ["Technology", "Business", "Travel"];
+  private selectedCategory: string = "Technology";
+}
 </script>
